7924e4998b14b5a7674d61f90d7c428b1a7e2db0
4jchc <jiangjin5y24@163.com>
2015-12-26 16:15:47 +0800

    1.3 添加自定义Modal动画

---
+++ 01-UIAlertPopoverPresentationController/01-UIAlertController/ThreeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ThreeViewController.swift
+//  01-UIAlertPopoverPresentationController
+//
+//  Created by 蒋进 on 15/12/26.
+//  Copyright © 2015年 sijichcai. All rights reserved.
+//
+
+import UIKit
+
+class ThreeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
